fa90f46edbe70d077b337c3c26fb27765e9880a8
xerxiastudios <xerxiastudios@gmail.com>
2023-08-02 14:24:51 +0530

    Working trpc

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -2,9 +2,14 @@ import {
   drizzle as drizzleDb,
   PostgresJsDatabase,
 } from "drizzle-orm/postgres-js";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 
-const client = postgres(
+const migrationConnection = postgres(
+  process.env.DATABASE_URL || "postgres://localhost:5432/drizzle",
+  { max: 1 }
+);
+const queryConnection = postgres(
   process.env.DATABASE_URL || "postgres://localhost:5432/drizzle"
 );
 
@@ -14,8 +19,20 @@ declare global {
   var drizzle: DrizzleClient;
 }
 
-export const drizzle: DrizzleClient = global.drizzle || drizzleDb(client);
+export const drizzle: DrizzleClient =
+  global.drizzle || drizzleDb(queryConnection);
 
 if (process.env.NODE_ENV !== "production") {
   global.drizzle = drizzle;
 }
+
+const main = async () => {
+  await migrate(drizzleDb(migrationConnection), {
+    migrationsFolder: "drizzle",
+  });
+  await migrationConnection.end();
+  console.log("migration done");
+  process.exit(0);
+};
+
+main();
